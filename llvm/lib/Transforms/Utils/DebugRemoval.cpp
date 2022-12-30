99bfbb17831ef55e041d0bfaa28fa3df7048ded1
Jovan Dmitrovic <Jovan.Dmitrovic@Syrmia.com>
2022-12-30 10:55:24 +0100

    Cleaning up DebugRemoval pass

--- llvm/lib/Transforms/Utils/DebugRemoval.cpp
+++ llvm/lib/Transforms/Utils/DebugRemoval.cpp
@@ -6,16 +6,14 @@ PreservedAnalyses DebugRemovalPass::run(Function &F, FunctionAnalysisManager &AM
     SmallVector<Instruction*, 128> DebugI;
 
     for(inst_iterator I = inst_begin(F), End = inst_end(F); I != End; ++I) {
-        if(I->isDebugOrPseudoInst() && isa<CallInst>(*I)) {
-            // I->dropAllReferences();
-            // I->removeFromParent();
+        if(I->isDebugOrPseudoInst()) {
             DebugI.push_back(&*I);
         }
     }
 
     for(Instruction* I : DebugI) {
         I->dropAllReferences();
-        I->removeFromParent();
+        I->eraseFromParent();
     }
 
     return PreservedAnalyses::none();
