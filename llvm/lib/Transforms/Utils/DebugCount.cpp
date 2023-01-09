ba6db76b08353368a1c73daa236ad0aa3339c7a9
Jovan Dmitrovic <Jovan.Dmitrovic@Syrmia.com>
2023-01-09 15:09:41 +0100

    Changed from std::map to StringMap

--- llvm/lib/Transforms/Utils/DebugCount.cpp
+++ llvm/lib/Transforms/Utils/DebugCount.cpp
@@ -1,19 +1,21 @@
 #include "llvm/Transforms/Utils/DebugCount.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/ADT/StringMap.h"
 
 using namespace llvm;
 
 PreservedAnalyses DebugCountPass::run(Function &F, FunctionAnalysisManager &AM) {
-    std::unordered_map<const char*, int> DebugMap;
+    StringMap<int> DebugMap;
 
     for(inst_iterator I = inst_begin(F), End = inst_end(F); I != End; ++I) {
         if(I->isDebugOrPseudoInst() && isa<CallInst>(*I)) {
             CallInst& CI = cast<CallInst>(*I);
-            DebugMap[CI.getCalledFunction()->getName().data()]++;
+            DebugMap[CI.getCalledFunction()->getName()]++;
         }
     }
 
-    for(auto I : DebugMap) {
-        llvm::outs() << I.first << ':' << I.second << '\n';
+    for(auto &I : DebugMap) {
+        llvm::outs() << I.getKey() << ':' << I.getValue() << '\n';
     }
 
     return PreservedAnalyses::all();
