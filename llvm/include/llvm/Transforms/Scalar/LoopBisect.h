4dcdac3e5dff8751c8641c13583698537ac09f65
Jovan Dmitrovic <Jovan.Dmitrovic@Syrmia.com>
2023-01-12 10:30:36 +0100

    Added LoopBisect splitting and cloning

--- llvm/include/llvm/Transforms/Scalar/LoopBisect.h
+++ llvm/include/llvm/Transforms/Scalar/LoopBisect.h
@@ -1,25 +1,62 @@
 #ifndef LLVM_TRANSFORMS_SCALAR_LOOPBISECT_H
 #define LLVM_TRANSFORMS_SCALAR_LOOPBISECT_H
 
+#include "llvm/Pass.h"
 #include "llvm/Analysis/LoopAnalysisManager.h"
-
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/LoopPass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/Transforms/Utils/ValueMapper.h"
 
 namespace llvm {
 class Loop;
 class LPMUpdater;
 
+// This class divides innermost loop into two loops.
 class LoopBisect {
 public:
-    LoopBisect(LoopInfo &LI) : LI(LI) {}
+    LoopBisect(LoopInfo &LI, ScalarEvolution &SE, DominatorTree &DT)
+        : LI(LI), SE(SE), DT(DT) {}
 
     bool run(Loop &L) const;
 
 private:
     LoopInfo &LI;
+    ScalarEvolution &SE;
+    DominatorTree &DT;
 
     // Determines if a provided loop is a candidate
     // for bisecting
     bool isCandidate(const Loop &L) const;
+
+    // Splits loop in half by traversing halfway through
+    // a loop, then cloning the loop while adjusting
+    // loop bounds of both original and cloned loops.
+    bool splitLoopInHalf(Loop &L) const;
+
+    // Clone loop and insert it before InsertBefore basic block.
+    // Pred represents the predecessor of loop L.
+    Loop *cloneLoop(Loop &L, BasicBlock &InsertBefore, BasicBlock &Pred) const;
+
+
+    // Compute the point of splitting of loop L. Instruction that is returned
+    // calculates the split point.
+    Instruction *computeSplitPoint(const Loop &L,
+                                   Instruction *InsertBefore) const;
+
+    // Get latch comparison instruction.
+    ICmpInst *getLatchCmpInst(const Loop &L) const;
+
+    // Update DominatorTree after cloning loop.
+    void updateDominatorTree(const Loop &OrigLoop, const Loop &ClonedLoop,
+                             BasicBlock &InsertBefore, BasicBlock &Pred,
+                             ValueToValueMapTy &VMap) const;
+
+    void dumpFunction(const StringRef Msg, const Function &F) const;
 };
 
 class LoopBisectPass : public PassInfoMixin<LoopBisectPass> {
--- llvm/lib/Transforms/Scalar/LoopBisect.cpp
+++ llvm/lib/Transforms/Scalar/LoopBisect.cpp
@@ -1,13 +1,10 @@
 #include "llvm/Transforms/Scalar/LoopBisect.h"
-#include "llvm/Analysis/LoopPass.h"
-#include "llvm/Analysis/LoopInfo.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
 
 using namespace llvm;
 
 #define DEBUG_TYPE "loop-bisect"
+
+static const char *VerboseDebug = DEBUG_TYPE "-verbose";
 
 bool LoopBisect::run(Loop &L) const {
     LLVM_DEBUG(dbgs() << "Entering " << __func__ << '\n');
@@ -22,7 +19,7 @@ bool LoopBisect::run(Loop &L) const {
     LLVM_DEBUG(dbgs() << "Loop: " << L.getName()
                 << "is a candidate for bisecting");
 
-    return true;
+    return splitLoopInHalf(L);
 }
 
 bool LoopBisect::isCandidate(const Loop &L) const {
@@ -49,17 +46,135 @@ bool LoopBisect::isCandidate(const Loop &L) const {
     return true;
 }
 
+bool LoopBisect::splitLoopInHalf(Loop &L) const {
+    assert(L.isLoopSimplifyForm() && "Loop needs to be in simplified form");
+    assert(L.isSafeToClone() && "Loop must be cloneable");
+    assert(L.getSubLoops().empty() && "Only innermost loop can be split");
+
+    LLVM_DEBUG(dbgs() << "Splitting loop " << L.getName() << "\n");
+
+    BasicBlock *Preheader = L.getLoopPreheader();
+    Instruction *Split = computeSplitPoint(L, Preheader->getTerminator());
+    BasicBlock *Pred = Preheader;
+    BasicBlock *InsertBefore = SplitBlock(Preheader, Preheader->getTerminator(),
+                                          &DT, &LI);
+
+    const Function &F = *L.getHeader()->getParent();
+    DEBUG_WITH_TYPE(VerboseDebug, dumpFunction("After splitting preheader:\n", F));
+
+    // Loop *ClonedLoop =
+
+    return true;
+}
+
+void LoopBisect::dumpFunction(const StringRef Msg, const Function &F) const {
+    dbgs() << Msg;
+    F.dump();
+}
+
+Loop *myCloneLoopWithPreheader(BasicBlock *Before, BasicBlock *LoopDomBB,
+                               Loop *OrigLoop, ValueToValueMapTy &VMap,
+                               const Twine &NameSuffix, LoopInfo *LI,
+                               SmallVectorImpl<BasicBlock *> &Blocks);
+
+Loop *LoopBisect::cloneLoop(Loop &L, BasicBlock &InsertBefore, BasicBlock &Pred) const {
+    Function &F = *L.getHeader()->getParent();
+    SmallVector<BasicBlock *, 4> ClonedLoopBlocks;
+    ValueToValueMapTy VMap;
+
+    Loop * NewLoop = myCloneLoopWithPreheader(&InsertBefore, &Pred, &L, VMap, "",
+                                              &LI, ClonedLoopBlocks);
+
+    assert(NewLoop && "Ran out of memory");
+    DEBUG_WITH_TYPE(VerboseDebug,
+                    dbgs() << "Created new loop: " << NewLoop->getName() << "\n";
+                    dumpFunction("After cloning loop:\n", F););
+
+    // Update instructions referencing the original loop basic blocks to
+    // reference the corresponding block in the cloned loop.
+    VMap[L.getExitBlock()] = &InsertBefore;
+    remapInstructionsInBlocks(ClonedLoopBlocks, VMap);
+    DEBUG_WITH_TYPE(VerboseDebug,
+                    dumpFunction("After instruction remapping:\n", F));
+
+    return NewLoop;
+}
+
 PreservedAnalyses LoopBisectPass::run(Loop &L, LoopAnalysisManager &AM,
                                       LoopStandardAnalysisResults &AR,
                                       LPMUpdater &U) {
     LLVM_DEBUG(dbgs() << "Entering LoopBisectPass::run\n");
     LLVM_DEBUG(dbgs() << "Loop: "; L.dump(); dbgs() << "\n");
 
-    bool Changed = LoopBisect(AR.LI).run(L);
+    bool Changed = LoopBisect(AR.LI, AR.SE, AR.DT).run(L);
 
     if(!Changed) {
         return PreservedAnalyses::all();
     }
 
     return llvm::getLoopPassPreservedAnalyses();
+}
+
+Loop *myCloneLoopWithPreheader(BasicBlock *Before, BasicBlock *LoopDomBB,
+                               Loop *OrigLoop, ValueToValueMapTy &VMap,
+                               const Twine &NameSuffix, LoopInfo *LI,
+                               SmallVectorImpl<BasicBlock *> &Blocks) {
+    Function *F = OrigLoop->getHeader()->getParent();
+    Loop *ParentLoop = OrigLoop->getParentLoop();
+    DenseMap<Loop *, Loop *> LMap;
+
+    Loop *NewLoop = LI->AllocateLoop();
+    LMap[OrigLoop] = NewLoop;
+
+    if(ParentLoop)
+        ParentLoop->addChildLoop(NewLoop);
+    else
+        LI->addTopLevelLoop(NewLoop);
+
+    BasicBlock *OrigPH = OrigLoop->getLoopPreheader();
+    assert(OrigPH && "No preheader in original loop");
+    BasicBlock *NewPH = CloneBasicBlock(OrigPH, VMap, NameSuffix, F);
+
+    VMap[OrigPH] = NewPH;
+    Blocks.push_back(NewPH);
+
+    // Update LoopInfo.
+    if(ParentLoop)
+        ParentLoop->addBasicBlockToLoop(NewPH, *LI);
+
+    for(Loop *CurrLoop : OrigLoop->getLoopsInPreorder()) {
+        Loop *&NewLoop = LMap[CurrLoop];
+
+        if(!NewLoop) {
+            NewLoop = LI->AllocateLoop();
+
+            Loop *OrigParentLoop = CurrLoop->getParentLoop();
+            assert(OrigParentLoop && "Could not find the original parent loop");
+            Loop *NewParentLoop = LMap[OrigParentLoop];
+            assert(NewParentLoop && "Could not find the new parent loop");
+
+            NewParentLoop->addChildLoop(NewLoop);
+        }
+    }
+
+    for(BasicBlock *BB : OrigLoop->getBlocks()) {
+        Loop *CurrLoop = LI->getLoopFor(BB);
+        Loop *&NewLoop = LMap[CurrLoop];
+        assert(NewLoop && "New loop is expected to be allocated");
+
+        BasicBlock *NewBB = CloneBasicBlock(BB, VMap, NameSuffix, F);
+        VMap[BB] = NewBB;
+
+        NewLoop->addBasicBlockToLoop(NewBB, *LI);
+        if(BB == CurrLoop->getHeader())
+            NewLoop->moveToHeader(NewBB);
+
+        Blocks.push_back(NewBB);
+    }
+
+    F->splice(Before->getIterator(), F, NewPH->getIterator());
+    F->splice(Before->getIterator(), F, NewLoop->getHeader()->getIterator(),
+              F->end());
+
+    return NewLoop;
 }
